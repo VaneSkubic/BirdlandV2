e803f035b7f33bfa8e4fdb088b55a501cce7e548
VaneSkubic <vane.skubic@gmail.com>
2021-08-27 19:03:00 +0200

    hamburger menu for phone

---
+++ script.js
@@ -0,0 +1,25 @@
+function hamburgerMenu() {
+    var nav = document.getElementById("nav");
+    var cross = document.getElementById("cross");
+    var hamburger = document.getElementById("hamburger");
+    var phoneNav = document.getElementById("phone-nav");
+    var header = document.getElementById("header");
+    if (nav.style.display === "flex") {
+        phoneNav.style.height = '15vh';
+        phoneNav.style.alignItems = 'flex-start';
+        cross.style.display = 'none';
+        hamburger.style.display = 'inline';
+        nav.style.display = "none";
+        header.style.backgroundColor = '#00000000';
+    } else {
+        phoneNav.style.height = '10vh';
+        phoneNav.style.alignItems = 'center';
+        cross.style.display = 'inline';
+        hamburger.style.display = 'none';
+        nav.style.display = "flex";
+        header.style.backgroundColor = '#242424';
+    }
+
+
+
+}
